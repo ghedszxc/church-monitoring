962eee70739de6fe028bae4570765dc2468767b2
Ghed Mendiola <mendiolagerard96@gmail.com>
2025-01-06 11:11:24 +0800

    implement initial GET request

--- back-end/app.js
+++ back-end/app.js
@@ -11,20 +11,75 @@ app.listen(PORT, (error) =>{
     }
 );
 
-app.get('/', (req, res)=>{
-    res.status(200);
-    res.send("Welcome to root URL of Server");
+
+// const express = require('express');
+const router = express.Router();
+const connection = require('./db_connection/index.js');
+
+
+
+app.get('/disciple', (req, res)=>{
+    // res.set('Content-Type', 'text/html');
+    // res.status(200).send("<h1>Hello there!</h1>");
+
+  connection.connect();
+
+    connection.query('SELECT * FROM `disciples`', function (error, results, fields) {
+      // error will be an Error if one occurred during the query
+      // results will contain the results of the query
+      // fields will contain information about the returned results fields (if any)
+      console.log('results: ', results[0]);
+    });
+
+  connection.end();
 });
 
-app.get('/hello', (req, res)=>{
-    res.set('Content-Type', 'text/html');
-    res.status(200).send("<h1>Hello GFG Learner!</h1>");
-});
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   // res.render('layout', { title: 'Movieseat' });
+
+//   connection.connect();
+
+//     connection.query('SELECT * FROM `users` WHERE `first_name` = "Kees"', function (error, results, fields) {
+//       // error will be an Error if one occurred during the query
+//       // results will contain the results of the query
+//       // fields will contain information about the returned results fields (if any)
+//       console.log(results);
+//     });
+
+//   connection.end();
+// });
+
+// module.exports = router;
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'bocaue_masterlist'
+// })
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+// connection.end()
 
 
-app.use(express.json());
-app.post('/', (req, res)=>{
-    const {name} = req.body;
+
+// app.get('/', (req, res)=>{
+//     res.status(200);
+//     res.send("Welcome to root URL of Server");
+// });
+
+// app.use(express.json());
+// app.post('/', (req, res)=>{
+//     const {name} = req.body;
     
-    res.send(`Welcome ${name}`);
-})
+//     res.send(`Welcome ${name}`);
+// })
