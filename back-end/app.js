562c035f503d85dbc92d2611a4c2ac5165a9e781
Ghed Mendiola <mendiolagerard96@gmail.com>
2025-01-14 18:27:50 +0800

    implement env

--- back-end/app.js
+++ back-end/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: false }));
