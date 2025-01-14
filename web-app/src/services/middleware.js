27daf8c4fba4192a334cd1bdaafe06310d1e019f
Ghed Mendiola <mendiolagerard96@gmail.com>
2025-01-14 19:11:42 +0800

    update middleware

--- web-app/src/services/middleware.js
+++ web-app/src/services/middleware.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:5000'
+        // baseURL: 'http://localhost:5000',
+        baseURL: 'https://web-bocaue-api.onrender.com'
     })
 }
