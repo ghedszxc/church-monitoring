17c0d0911a26f356dec1eba1d22787c3c6455677
Ghed Mendiola <mendiolagerard96@gmail.com>
2025-01-14 19:31:05 +0800

    update base url

--- web-app/src/services/middleware.js
+++ web-app/src/services/middleware.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         // baseURL: 'http://localhost:5000',
-        baseURL: 'https://web-bocaue-api.onrender.com'
+        baseURL: 'https://web-api-t9vp.onrender.com'
     })
 }
