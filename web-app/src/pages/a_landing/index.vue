3bbf39e55029d2033650fb70c0f0d0dbc2f872d8
Gerard <mendiolagerard96@gmail.com>
2026-01-19 22:28:36 +0800

    link

--- web-app/src/pages/a_landing/index.vue
+++ web-app/src/pages/a_landing/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="mt-1">
-    Darwin Bading!
+    HNeed Bading?
+    <a href="https://www.facebook.com/darwin.palisoc.2024" class="text-blue-500"
+      >Click here to hire</a
+    >
     <!-- <ImageBanner :url="'fd-26.jpg'" :alt="'faith declaration 2026'" /> -->
   </div>
 </template>
