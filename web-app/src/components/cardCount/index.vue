ea9708fb19c7ff6fb060f8f67f6f71fa0ba8606f
Gerard <mendiolagerard96@gmail.com>
2026-01-20 14:32:14 +0800

    update landing page

---
+++ web-app/src/components/cardCount/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="rounded-lg shadow-lg grid place-items-center py-3">
+    <span>{{ data?.count }}</span>
+
+    <!-- <NumberAnimation
+      ref="number1"
+      :from="100"
+      :to="10000"
+      :format="theFormat"
+      :duration="5"
+      autoplay
+      easing="linear"
+    /> -->
+    <i :class="`pi ${data?.icon} text-red-500 text-[4rem] my-2`"></i>
+    <h3>
+      {{ data?.text }}
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+}
+</script>
--- web-app/src/pages/a_landing/index.vue
+++ web-app/src/pages/a_landing/index.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="mt-1">
+  <div class="mt-12 w-[95vw] lg:w-[75vw] place-self-center">
     <!-- <ImageBanner :url="'fd-26.jpg'" :alt="'faith declaration 2026'" /> -->
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <template v-for="(data, key) in items" :key="key">
+        <CardCount :data="data" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 import ImageBanner from '@/components/imageBanner/index.vue'
+import CardCount from '@/components/cardCount/index.vue'
 export default {
   components: {
     ImageBanner,
+    CardCount,
   },
+  data: () => ({
+    items: [
+      { text: 'Registered Disciples', icon: 'pi-users', count: 75 },
+      { text: 'Active Disciples', icon: 'pi-user', count: 65 },
+      { text: 'Registered Leaders', icon: 'pi-android', count: 35 },
+      { text: 'Active Leaders', icon: 'pi-apple', count: 25 },
+    ],
+  }),
 }
 </script>
