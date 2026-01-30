f3b9bafc3a743c6bdc66c50c0c3838518c8b4184
Gerard <mendiolagerard96@gmail.com>
2026-01-30 17:11:21 +0800

    display fd 2026

--- web-app/src/pages/a_landing/index.vue
+++ web-app/src/pages/a_landing/index.vue
@@ -1,22 +1,25 @@
 <template>
-  <div
-    class="mt-12 w-[95vw] lg:w-[85vw] xl:w-[60vw] place-self-center grid grid-cols-1 md:grid-cols-2 gap-8 xl:gap-12 px-3"
-  >
-    <!-- <ImageBanner :url="'fd-26.jpg'" :alt="'faith declaration 2026'" /> -->
-    <Birthday />
+  <div class="mt-12 w-[95vw] lg:w-[85vw] xl:w-[60vw] place-self-center px-2">
+    <div class="my-6 lg:my-12">
+      <img src="@/assets/images/fd-26.jpg" class="h-full w-full" />
+    </div>
 
-    <div>
-      <h2 class="text-3xl uppercase text-[#ffffff]">.</h2>
-      <div class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-4">
-        <template v-for="(data, key) in items" :key="key">
-          <CardCount :data="data" :index="key">
-            <template #actualCount>{{ data?.count }}</template>
-            <template #icon
-              ><i :class="`pi ${data?.icon} text-red-500 text-[2rem] mr-3`"
-            /></template>
-            <template #subtitle>{{ data?.text }}</template>
-          </CardCount>
-        </template>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 xl:gap-12">
+      <Birthday />
+
+      <div>
+        <h2 class="text-[1.7rem] uppercase text-[#ffffff] hidden md:inline">.</h2>
+        <div class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-4">
+          <template v-for="(data, key) in items" :key="key">
+            <CardCount :data="data" :index="key">
+              <template #actualCount>{{ data?.count }}</template>
+              <template #icon
+                ><i :class="`pi ${data?.icon} text-red-500 text-[2rem] mr-3`"
+              /></template>
+              <template #subtitle>{{ data?.text }}</template>
+            </CardCount>
+          </template>
+        </div>
       </div>
     </div>
   </div>
@@ -24,7 +27,6 @@
 
 <script setup>
 import { ref } from 'vue'
-import ImageBanner from '@/components/imageBanner/index.vue'
 import CardCount from '@/components/cardCount/index.vue'
 import Birthday from '@/components/birthday/index.vue'
 
@@ -32,16 +34,4 @@ const items = ref([
   { text: 'Disciples', icon: 'pi-users', count: 75 },
   { text: 'Leaders', icon: 'pi-user', count: 35 },
 ])
-// export default {
-//   components: {
-//     ImageBanner,
-//     CardCount,
-//   },
-//   data: () => ({
-//     items: [
-//       { text: 'Disciples', icon: 'pi-users', count: 75 },
-//       { text: 'Leaders', icon: 'pi-user', count: 35 },
-//     ],
-//   }),
-// }
 </script>
