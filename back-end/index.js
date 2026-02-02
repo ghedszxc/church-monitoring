090740b3d598fd2dc4ca9cfd9372207b2e806d61
Gerard <mendiolagerard96@gmail.com>
2026-02-02 19:05:37 +0800

    add api request

--- back-end/index.js
+++ back-end/index.js
@@ -8,13 +8,14 @@ const port = 3000;
 // Routes
 // const user = require("./src/routes/user");
 // const disciple = require("./src/routes/disciple");
+const disciple = require("./src/routes/disciple");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use("/api/users", user);
-// app.use("/api/disciples", disciple);
+app.use("/api/disciples", disciple);
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!! <3");
--- back-end/src/controllers/disciple.js
+++ back-end/src/controllers/disciple.js
@@ -12,42 +12,42 @@ class DiscipleController {
     }
   }
 
-  static async getDisciple(req, res) {
-    try {
-      const { id } = req.params;
-      const disciple = await Disciple.getDiscipleById(id);
-      disciple
-        ? res.json(disciple)
-        : res.status(404).json({ message: "Disciple not found" });
-    } catch (error) {
-      console.error("Error fetching disciple:", error);
-      res.status(500).json({ error: error?.message });
-    }
-  }
+  // static async getDisciple(req, res) {
+  //   try {
+  //     const { id } = req.params;
+  //     const disciple = await Disciple.getDiscipleById(id);
+  //     disciple
+  //       ? res.json(disciple)
+  //       : res.status(404).json({ message: "Disciple not found" });
+  //   } catch (error) {
+  //     console.error("Error fetching disciple:", error);
+  //     res.status(500).json({ error: error?.message });
+  //   }
+  // }
 
-  static async createDisciple(req, res) {
-    try {
-      const { givenName, middleName, surname, network, birthdate, status } =
-        req.body;
+  // static async createDisciple(req, res) {
+  //   try {
+  //     const { givenName, middleName, surname, network, birthdate, status } =
+  //       req.body;
 
-      const newDiscipleId = await Disciple.createDisciple(
-        givenName,
-        middleName,
-        surname,
-        network,
-        birthdate,
-        status,
-      );
+  //     const newDiscipleId = await Disciple.createDisciple(
+  //       givenName,
+  //       middleName,
+  //       surname,
+  //       network,
+  //       birthdate,
+  //       status,
+  //     );
 
-      res.status(201).json({
-        id: newDiscipleId,
-        message: `${givenName} ${surname} is successfully added from disciple list`,
-      });
-    } catch (error) {
-      console.error("Error creating disciple:", error);
-      res.status(500).json({ error: error?.message });
-    }
-  }
+  //     res.status(201).json({
+  //       id: newDiscipleId,
+  //       message: `${givenName} ${surname} is successfully added from disciple list`,
+  //     });
+  //   } catch (error) {
+  //     console.error("Error creating disciple:", error);
+  //     res.status(500).json({ error: error?.message });
+  //   }
+  // }
 }
 
 module.exports = DiscipleController;
