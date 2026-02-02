36397252a25a0acf4f0e9c1035b1eb797560fe9b
Gerard <mendiolagerard96@gmail.com>
2026-02-02 19:13:29 +0800

    remove apis

--- back-end/index.js
+++ back-end/index.js
@@ -6,13 +6,13 @@ const app = express();
 const port = 3000;
 
 // Routes
-const disciple = require("./src/routes/disciple");
+// const disciple = require("./src/routes/disciple");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/disciples", disciple);
+// app.use("/api/disciples", disciple);
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!! ????");
